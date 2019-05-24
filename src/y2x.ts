4e22c47c364ee77e29cd90e26a3b8dce9d624e3b
Mauricio Navarro Miranda <mauricio@navarromiranda.mx>
2019-05-23 19:01:39 -0500

    y2x

--- src/y2x.ts
+++ src/y2x.ts
@@ -1,11 +1,23 @@
+import Bitcoin = require('bitcoinjs-lib');
+import b58 = require('bs58check');
+
 export class y2x {
-  private greeting: string;
 
-  constructor(message: string) {
-    this.greeting = message;
+  public static yPubOrNot(yPub: string): boolean {
+    const version = b58.decode(yPub).slice(0, 4);
+    return version.equals(Buffer.from('049d7cb2', 'hex'));
   }
 
-  public greet(): string {
-    return `Bonjour, ${this.greeting}!`;
+  public static hNode2SegwitP2sh(hdNode: Bitcoin.HDNode): string {
+    const pubkeyBuf = hdNode.keyPair.getPublicKeyBuffer();
+    const hash = Bitcoin.crypto.hash160(pubkeyBuf);
+    const redeemScript = Bitcoin.script.witnessPubKeyHash.output.encode(hash);
+    const hash2 = Bitcoin.crypto.hash160(redeemScript);
+    const scriptPubkey = Bitcoin.script.scriptHash.output.encode(hash2);
+    return Bitcoin.address.fromOutputScript(scriptPubkey);
+
   }
+
+  constructor() { }
+
 }
